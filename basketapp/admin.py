5ed5b09949b6aa5f8792c6e337400b84208d9431
Daniel Montecinos <danielmontecinoszambra@gmail.com>
2018-04-17 21:08:44 -0300

    migrations de modelos y super user creado, daniel daniel123 user/pass

--- basketapp/admin.py
+++ basketapp/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+
+from basketapp.models import Team, Player, Coach, Match
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['logo']
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ['photograpy']
+    list_filter = ['name','birthday']
+
+@admin.register(Coach)
+class CoachAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ['namematch']
+
+
+# @admin.register(Song)
+# class SongAdmin(admin.ModelAdmin):
+#     list_display = ('name','duration','album')
+#     list_filter = ('album','duration')
--- basketapp/models.py
+++ basketapp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Team(models.Model):
+    nombre = models.CharField(max_length=200)
+    description = models.TextField()
+    logo = models.ImageField()
+    codeTeam = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Player(models.Model):
+    name = models.CharField(max_length=200)
+    alias = models.CharField(max_length=200)
+    birthday =models.DateField()
+    age = models.IntegerField()
+    rut = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    height = models.FloatField()
+    weight = models.FloatField()
+    photograpy = models.ImageField()
+    position = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Coach(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    email = models.CharField(max_length=200)
+    rut = models.CharField(max_length=200)
+    alias = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+class Match(models.Model):
+    namematch = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
