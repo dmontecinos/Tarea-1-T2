00831154ad2f682d2a3be95713d0983afff453a7
Daniel Montecinos <danielmontecinoszambra@gmail.com>
2018-04-20 20:26:50 -0300

    eliminacion de archivos basura de prueba y cambio en match namematch

--- basketapp/models.py
+++ basketapp/models.py
@@ -50,4 +50,4 @@ class Coach(models.Model):
 class Match(models.Model):
     namematch = models.CharField(max_length=200)
     def __str__(self):
-        return self.name
+        return self.namematch
